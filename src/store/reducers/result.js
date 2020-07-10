620e846f0ac45429c69e4ee2cc99690efb78ab5f
draganasavanovic <dragana.savanovic@wayseven.com>
2020-07-10 13:36:47 +0200

    a leaner switch case statement

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,30 +1,29 @@
 import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "../utility";
 
 const initialState = {
   results: [],
 };
 
+const deleteResult = (state, action) => {
+  const updatedArray = state.results.filter(
+    (result) => result.id !== action.resultElementId
+  );
+  return updateObject(state, { results: updatedArray });
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-      return {
-        ...state,
+      return updateObject(state, {
         results: state.results.concat({
           id: new Date(),
-          value: action.result,
+          value: action.result * 2,
         }),
-      };
+      });
     case actionTypes.DELETE_RESULT:
-      // const id = 2;
-      // const newArray = [...state.results];
-      // newArray.splice(id, 1);
-      const updatedArray = state.results.filter(
-        (result) => result.id !== action.resultElementId
-      );
-      return {
-        ...state,
-        results: updatedArray,
-      };
+      return deleteResult(state, action);
+
     default:
       return state;
   }
